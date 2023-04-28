f7ba11fc192d54e2286355587000aa0834745197
sujitmemane <officialsujitmemane@gmail.com>
2023-04-28 15:55:31 +0530

    FINAL

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,11 +6,10 @@ const options = {
     gl: "US",
   },
   headers: {
-    "X-RapidAPI-Key": "6d9119f36emsh3cc235e179fc887p14c2f5jsnb2d042e9c309",
+    "X-RapidAPI-Key": "5e40a646b0mshaa888b22d9c4612p1b5afajsn918783b7449f",
     "X-RapidAPI-Host": "youtube-v38.p.rapidapi.com",
   },
 };
-
 const fetchDataFromAPI = async (url) => {
   const response = await fetch(`${Base_URL}/${url}`, options);
   const data = response.json();
